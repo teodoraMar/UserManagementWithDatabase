11b73529ae5aa365ff8a82dfec1951a4e6e366e0
teodoraMar <dodamarjanovic@yahoo.com>
2022-09-09 10:33:54 +0200

    added json util

---
+++ src/main/java/com/example/UserManagementWithDatabase/util/JsonUtil.java
@@ -0,0 +1,26 @@
+package com.example.UserManagementWithDatabase.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+    public static String objectToJson(Object object) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static <T> T fromJson(String json, Class<T> type) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(json, type);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
